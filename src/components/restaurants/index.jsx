820111cf48e158fa9df2b4ff5357d793b70b38cf
Sergo588 <mazmanyan_1990@mail.ru>
2024-02-14 00:35:38 +0300

    fix

--- src/components/restaurants/index.jsx
+++ src/components/restaurants/index.jsx
@@ -1,9 +1,9 @@
 import React, { Children } from "react";
-
+import styles from "./styles.module.scss";
 
 export const Restaurants = ({rests}) => {
     return (
-        <div>
+        <div className={styles.root}>
             <h1>{rests.name}</h1>  
             <h3>{rests.menu[0].name}</h3>
             <h3>{rests.menu[0].price}</h3>  
